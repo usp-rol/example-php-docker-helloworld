7810d8224412547448a66e18e550d6a957e5a99f
Rolf Linder <rolf.linder@united-security-providers.ch>
2018-07-05 16:10:31 +0200

    updated message techlab

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo <?php echo 'APPUiO'?></h1>
+        <h1>Hello <?php echo 'APPUiO'?></h1>
         <p class="lead">APPUiO Example Dockerfile PHP</p>
       </div>
 
